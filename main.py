7d6fea046127dca5003974f235d2d41c0195c1ee
Ez <ezequi.santos.pereira@gmail.com>
2025-11-13 14:33:21 -0300

    teste 2

--- main.py
+++ main.py
@@ -3,9 +3,4 @@ from comandos.teste import oi
 while True:
     command = input('Digite um comando: ')
 
-    command.split(' ')
-
-    indice = 0
-
-    for params in command:
-        print(params)
+    print(command)
