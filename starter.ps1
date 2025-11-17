971f0df2f5aa31b6c043e7fad4f26cd53d0c43e2
Ez <ezequi.santos.pereira@gmail.com>
2025-11-17 14:54:02 -0300

    teste

--- starter.ps1
+++ starter.ps1
@@ -1,10 +1,11 @@
 # definição de varialves para facil manuseio
-$app_url = "https://raw.githubusercontent.com/EzequielS4ntos/util-commands/main/app.zip"
+$app_url = "https://github.com/EzequielS4ntos/util-commands/archive/heads/main.zip"
 $zip_dir = "$env:TEMP\app.zip"
 $app_dir = "$env:TEMP\app"
 
 # se existir o path, remova-o
 if(Test-Path $zip_dir){Remove-Item $zip_dir} 
+if(Test-Path $app_dir){Remove-Item $app_dir} 
 
 # Requisição HTTP -arquivo de destino
 Invoke-RestMethod $app_url -OutFile $zip_dir
