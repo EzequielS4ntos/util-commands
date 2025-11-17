30229f958358c027960d845e7d842f26e704492c
Ez <ezequi.santos.pereira@gmail.com>
2025-11-17 14:47:04 -0300

    ps1 feito manualmente

--- starter.ps1
+++ starter.ps1
@@ -1,18 +1,15 @@
 # definição de varialves para facil manuseio
-$app_url = "https://github.com/EzequielS4ntos/util-commands/app.zip"
+$app_url = "https://raw.githubusercontent.com/EzequielS4ntos/util-commands/main/app.zip"
 $zip_dir = "$env:TEMP\app.zip"
 $app_dir = "$env:TEMP\app"
-
-# Requisição HTTP -arquivo de destino
-Invoke-RestMethod $app_url -OutFile $zip_dir
 
 # se existir o path, remova-o
 if(Test-Path $zip_dir){Remove-Item $zip_dir} 
 
-# cria o novo diretório zip | só para não exibir nada na tela
-New-Item -ItemType File -Path $zip_dir | Out-Null
+# Requisição HTTP -arquivo de destino
+Invoke-RestMethod $app_url -OutFile $zip_dir
 
 # Extrai o zip para o path de destino
 Expand-Archive -Path $zip_dir -DestinationPath $app_dir -Force
 
-python "$app_dir\main.py"
+python "$app_dir\util-commands-main\main.py"
