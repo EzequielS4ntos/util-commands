ea81df28886e352a7e7c3642fe43da4dc67733d3
Ez <ezequi.santos.pereira@gmail.com>
2025-11-17 15:42:46 -0300

    teste

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 from metods.hello import hello
+from time import sleep
 import os
 
 os.system('cls')
 os.system('color 02')
 
-print('===== teste =====')
+def digite(text, time = 0.05):
+    for char in text:
+        print(char, end='', flush=True)
+        sleep(time)
+
+msg = 'Script de serviço iniciado'
+msg2 = 'Seja bem vindo'
+
+digite(msg)
+
+digite('...', 1)
+
+sleep(3)
+
+os.system('cls')
+
+digite(msg2)
+
+sleep(2)
 
 while True:
+    os.system('cls')
     command = input('> ')
 
     if command == 'hello':
         os.system('cls')
         name = input('Insira seu nome\n> ')
+        print(hello(name))
+
+    
+    if command == 'exit':
+        break
+
+    if command == 'cls' or command == 'clear':
+        os.system('cls')
